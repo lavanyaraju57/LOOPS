5c93296ff6f45c6e816947d0b8b2c8a6472cc9bd
lavanyaraju57 <99262310+lavanyaraju57@users.noreply.github.com>
2022-03-26 11:01:59 +0530

    Add files via upload

---
+++ While_loop.py
@@ -0,0 +1,13 @@
+# s=int(input('enter the start value:'))
+# m=int(input('enter the end value:'))
+# while(s<=m):
+#     print(s)
+#     s+=5
+
+
+
+# s=int(input('enter the start value:'))
+# m=int(input('enter the end value:'))
+# while(s>=m):
+#     print(s)
+#     s-=5
