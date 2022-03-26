03fe8a6faa9a2c72f782c3edc968a103cec6d83d
lavanyaraju57 <99262310+lavanyaraju57@users.noreply.github.com>
2022-03-26 10:54:19 +0530

    Add files via upload

---
+++ If_statement.py
@@ -0,0 +1,95 @@
+# a=int(input("enter the value of a:"))
+# b=int(input("enter the value of b:"))
+# a,b=[int(x) for x in input().split()]
+# print(a,b)
+
+# a=int(input("enter the value of a:"))
+# if(a==5):
+#     print("yes equal")
+
+# a,b=[int(x) for x in input().split()]
+# if(a>b):
+#     print(f"{a}>{b} is {a>b}")
+# else:
+#     print(f"{a}>{b} is {a>b}")    
+
+# s,m=[int(x) for x in input().split()]
+# if(s>m):print("s is greater than m")
+# if(s<m):print("s is less than m")
+# if(s>=m):print("s is greater than or equal to m")
+# if(s<=m):print("s is less than or equal to m")
+# if(s!=m):print("s is not equal to m")
+# if(s==m):print("s is equal to or is same as m")
+
+
+# s,m=[int(x) for x in input().split()]
+# if(s>m):print("s is greater than m")
+# elif(s<m):print("s is less than m")
+# elif(s>=m):print("s is greater than or equal to m")
+# elif(s<=m):print("s is less than or equal to m")
+# elif(s!=m):print("s is not equal to m")
+# elif(s==m):print("s is equal to or is same as m")
+
+
+# a=int(input("enter the value of a:"))
+# b=int(input("enter the value of b:"))
+# if(a<b):
+#     print(f"{a} < {b} is true")
+
+# a=int(input("enter the value of a:"))
+# b=int(input("enter the value of b:"))
+# if(a<b):
+#     print(f"{a} < {b} is true")
+#     print("hey python")
+#     print("its done")
+
+
+# a=int(input("enter the value of a:"))
+# b=int(input("enter the value of b:"))
+# if(a<b):
+#     print(f"{a} < {b} is true")
+#     print("hey python")
+#     print("its done")
+# print("try next time")
+
+# a=int(input("enter the value of a:"))
+# b=int(input("enter the value of b:"))
+# if(a<b):
+#     print(f"{a} < {b} is true")
+# if(a<=b):
+#     print(f"{a} <= {b} is true")    
+# if(a>b):
+#     print(f"{a} > {b} is true")
+# if(a>b):
+#     print(f"{a} >= {b} is true")
+# if(a==b):
+#     print(f"{a} == {b} is true")
+
+
+# a=int(input("enter the value of a:"))
+# b=int(input("enter the value of b:"))
+# if(a<b):
+#     print(f"{a} is less than {b}")
+# else:
+#     print(f"{a} is greater than {b}")
+
+
+# s=int(input("enter the value of s:"))
+# m=int(input("enter the value of m:"))
+# if(s>m):print(f"{s} is greater than {m}")
+# elif(s<m):print(f"{s} is less than {m}")
+# elif(s>=m):print(f"{s} is greater than or equal to {m}")
+# elif(s<=m):print(f"{s} is less than or equal to {m}")
+# elif(s!=m):print(f"{s} is not equal to {m}")
+# elif(s==m):print(f"{s} is equal to or is same as {m}")
+# else:print("try again!")
+
+
+# n=int(input("enter any integer value:"))
+# if(n%2==0):
+#     print(f"{n} is an even number")
+# else:
+#     print(f"{n} is an odd number")
+
+
+
